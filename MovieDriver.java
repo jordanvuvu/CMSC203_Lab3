a63c9e5115756f4f491fcdf36cd1731846aaa974
jordanvuvu <70824761+jordanvuvu@users.noreply.github.com>
2020-09-20 11:07:52 -0400

    Add files via upload

--- MovieDriver.java
+++ MovieDriver.java
@@ -1,78 +1,30 @@
 import java.util.Scanner;
-
-class Movie {
-
-	private String name;
-	private String rating;
-	private int ticketSold;
-	
-	public Movie (String mName, String mRating, int mticketSold)
-	{super();
-	
-	name = mName;
-	rating = mRating;
-	ticketSold = mticketSold;
-	}
-	public String getName() {
-		return name;
-	}
-	
-	public void setName (String mName)
-	{
-		name = mName;
-	}
-	public String getRating() {
-		return rating;
-	}
-	
-	public void setRating(String mRating)
-	{
-		rating = mRating;
-	}
-	public int getticketSold() 
-	{
-		return ticketSold;
-	}
-	public void setticketSold(int mticketSold)
-	{
-		ticketSold = mticketSold;
-	}
-	public String toString()
-	{
-		return getName()+" ("+getRating()+") Tickets sold: "+ getticketSold();
-		
-	}
-}
-public class MovieDriver{
-	
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+public class MovieDriver {
+	public static void main(String[] arg) {
 		Scanner keyboard = new Scanner(System.in);
-		char ch = 'y';
-		String name="";
-		String rating="";
-		int tickets = 0;
-	
-	while (ch =='y' || ch=='Y') {
+		char choice ='y';
+		
+		while (choice=='y')
+		{
+		Movie nm= new Movie();
+		
+		
 		System.out.println("Enter the name of a movie: ");
-		name = keyboard.nextLine();
+		String title = keyboard.nextLine();
+		nm.setTitle(title);
+		System.out.println("Enter the rating of the movie: ");
+		String rating= keyboard.nextLine();
+		nm.setRating(rating);
+		System.out.println("Enter the number of tickets sold for this movie: ");
+		int soldTickets = keyboard.nextInt();
+		nm.setSoldTickets(soldTickets);
+		System.out.println(nm.toString());
 		
-		System.out.println("Enter the rating of movie: ");
-		rating = keyboard.nextLine();
-		
-		System.out.println("Ebter the number of tickets sold for this movie: ");
-		tickets= keyboard.nextInt();
-		
+		System.out.println("Do you want to enter another? (y or n)");
+		choice = keyboard.next().charAt(0);
 		keyboard.nextLine();
 		
-		Movie m = new Movie(name, rating, tickets);
-		System.out.println(m);
-		System.out.println("Do you want to enter another movie? (y or n) ");
-		ch = keyboard.nextLine().charAt(0);
 	}
-	System.out.println("Goodbye");
-	keyboard.close();
+		System.out.println("Goodbye");
 	}
 }
-		
-		
